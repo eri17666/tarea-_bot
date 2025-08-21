434248ab3438d07363f8decb25129e45325c33f0
ERICK1766 <guzmanquispeerickivan@gmail.com>
2025-08-21 09:46:36 -0400

    feat: Saludar dependiendo de la hora del día

--- src/presenter.js
+++ src/presenter.js
@@ -1,4 +1,17 @@
-document.addEventListener("DOMContentLoaded", () => {
+function obtenerSaludo() {
+  const fechaActual = new Date();
+  const horaActual = fechaActual.getHours();
+  
+  if (horaActual >= 6 && horaActual < 12) {
+    return "Buenos días";
+  } else if (horaActual >= 12 && horaActual < 18) {
+    return "Buenas tardes";
+  } else {
+    return "Buenas noches";
+  }
+}
+document.addEventListener("DOMContentLoaded", () => 
+  {
   const saludoSpan = document.getElementById('saludo-span');
-  saludoSpan.textContent = "Hola";
-});
+  saludoSpan.textContent = obtenerSaludo();;
+  });
